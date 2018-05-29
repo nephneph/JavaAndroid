f26c2d3620d1d3011135a8c99c1bce1fd48e71f9
akitomomiyauchi <csdn2wsx@live.jp>
2018-05-29 17:32:36 +0900

    android

---
+++ app/src/main/java/jp/learningdesign/javastudy/myfirstapp/MainActivity.java
@@ -0,0 +1,75 @@
+package jp.learningdesign.javastudy.myfirstapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.*;
+import android.text.Editable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickButton(View view){
+        TextView result =(TextView)this.findViewById(R.id.textView2);
+        EditText edittext_h = (EditText)this.findViewById(R.id.editText);
+        EditText edittext_w = (EditText)this.findViewById(R.id.editText2);
+
+        String s_height,s_weight;
+        s_height = edittext_h.getText().toString();
+        s_weight = edittext_w.getText().toString();
+
+        double bmi,height,weight;
+
+        height = Double.parseDouble(s_height);
+        weight = Double.parseDouble(s_weight);
+
+        height /= 100;
+        bmi = weight / (height * height);
+
+        String msg = "BMIの計算結果は"+ bmi + "です";
+        result.setText(msg);
+    }
+}
